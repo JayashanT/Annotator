9d0607fb1208317c09f5a9e47ff5334a42755848
jayashan.17@itfac.mrt.ac.lk <jayashan.17@itfac.mrt.ac.lk>
2021-09-01 12:28:44 +0530

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (<div>
+        <header style={{ textAlign:'center',fontWeight: 'bold', backgroundColor: 'blue', color: 'white', }}>
+            Racist Tweets Annotating tool
+        </header>
+    </div>);
+}
+
+export default Home
